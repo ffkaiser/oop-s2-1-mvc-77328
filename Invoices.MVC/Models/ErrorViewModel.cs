90695da01d30c896a515ded785cc4f936c3f25a6
felipe kaiser <felipekaiserjj@gmail.com>
2026-03-05 16:57:57 +0000

    ver1

---
+++ Invoices.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Invoices.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
